55e90f2a5c4172171ef00448636f6c4670baada3
Panda <cunhathiagoferreira@gmail.com>
2021-11-27 11:36:29 -0500

    Updated 1.0.5

--- src/index.js
+++ src/index.js
@@ -1,6 +1,43 @@
-const FSomar = require('./utils/somar');
-const FSubtrair = require('./utils/subtrair');
-const FMultiplicar = require('./utils/multiplicar');
-const FDividir = require('./utils/dividir')
+/** 
+* @param {number} algarismo1 Valor para somar.
+* @param {number} algarismo2 Valor para somar.
+*/
+function Somar(algarismo1, algarismo2) {
+    algarismo1 = Number(algarismo1)
+    algarismo2 = Number(algarismo2)
+    if (isNaN(algarismo1) || isNaN(algarismo2)) throw "Error: Valor informado não é um numero";
+    return algarismo1 + algarismo2
+}
+/** 
+* @param {number} algarismo1 Valor que será subtraído.
+* @param {number} algarismo2 Valor que irá subtrair.
+*/
+function Subtrair(algarismo1, algarismo2) {
+    algarismo1 = Number(algarismo1)
+    algarismo2 = Number(algarismo2)
+    if (isNaN(algarismo1) || isNaN(algarismo2)) throw "Error: Valor informado não é um numero";
+    return algarismo1 - algarismo2
+}
+/** 
+* @param {number} algarismo1 Valor para multiplicar.
+* @param {number} algarismo2 Valor para multiplicar.
+*/
+function Multiplicar(algarismo1, algarismo2) {
+    algarismo1 = Number(algarismo1)
+    algarismo2 = Number(algarismo2)
+    if (isNaN(algarismo1) || isNaN(algarismo2)) throw "Error: Valor informado não é um numero";
+    return algarismo1 * algarismo2
+}
+/** 
+* @param {number} algarismo1 Que será dividido.
+* @param {number} algarismo2 Valor que irá dividir.
+*/
+function Dividir(algarismo1, algarismo2) {
+    algarismo1 = Number(algarismo1)
+    algarismo2 = Number(algarismo2)
+    if (isNaN(algarismo1) || isNaN(algarismo2)) throw "Error: Valor informado não é um numero";
+    return algarismo1 / algarismo2
+}
 
-module.exports = {FSomar,FSubtrair,FMultiplicar,FDividir}
+
+module.exports = { Somar, Subtrair, Multiplicar, Dividir }
